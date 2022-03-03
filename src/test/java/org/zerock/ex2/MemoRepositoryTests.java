5e3296d6805b3bd1d664dd0881f12fa985d71f9e
test <test@test.com>
2022-03-03 22:20:44 +0900

    Initial commit

---
+++ src/test/java/org/zerock/ex2/MemoRepositoryTests.java
@@ -0,0 +1,18 @@
+package org.zerock.ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.ex2.repository.MemoRepository;
+
+@SpringBootTest
+public class MemoRepositoryTests {
+
+    @Autowired
+    MemoRepository memoRepository;
+
+    @Test
+    public void testClass() {
+        System.out.println(memoRepository.getClass().getName());
+    }
+}
